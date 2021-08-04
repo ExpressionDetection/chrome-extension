4a3e3ae70369a9bd71ea1cd27277b79c44072383
root <root@vagrant.vm>
2021-08-04 01:15:09 +0000

    WIP add zoom

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,10 +2,10 @@ chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
   try {
     // console.log("LOADED", changeInfo, tab);
     if (
-      /meet\.google\.com|teams\.live\.com/gi.test(tab.url) &&
+      /meet\.google\.com|teams\.live\.com|zoom\.us/gi.test(tab.url) &&
       changeInfo.status === "complete"
     ) {
-      // console.log("SENT", changeInfo, tab);
+      console.log("SENT", changeInfo, tab);
       chrome.tabs.sendMessage(
         tabId,
         { type: "expression-detection.onVideoConferenceTab", url: tab.url, tabId: tabId },
--- micro-facial-impressions/next.config.js
+++ micro-facial-impressions/next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+};
