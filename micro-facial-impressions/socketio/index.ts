5c47307f8607557b25b9e4a41bc88fba71b06b49
root <root@vagrant.vm>
2021-08-05 08:22:08 +0000

    Downgrading socket io from 4.1.3 to 2.3.1 in order to work without 101 connect/disconnect bug

--- micro-facial-impressions/socketio/index.ts
+++ micro-facial-impressions/socketio/index.ts
@@ -1,26 +1,22 @@
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 
 console.log("NEXT_PUBLIC_SOCKER_IO_PROTOCOL: ", process.env.NEXT_PUBLIC_SOCKER_IO_PROTOCOL)
-console.log("NEXT_PUBLIC_SOCKER_IO_HOST: ", process.env.NEXT_PUBLIC_SOCKER_IO_HOST)
-console.log("NEXT_PUBLIC_SOCKER_IO_PORT: ", process.env.NEXT_PUBLIC_SOCKER_IO_PORT)
+console.log("NEXT_PUBLIC_SOCKER_IO_HOST: ", process.env.NEXT_PUBLIC_SOCKER_IO_HOST)  
+console.log("NEXT_PUBLIC_SOCKER_IO_PORT: ", process.env.NEXT_PUBLIC_SOCKER_IO_PORT)  
 
 const URL = `${process.env.NEXT_PUBLIC_SOCKER_IO_PROTOCOL}://${process.env.NEXT_PUBLIC_SOCKER_IO_HOST}:${process.env.NEXT_PUBLIC_SOCKER_IO_PORT}`;
-const socket: any = io(URL, {
-  autoConnect: false,
-  transports: ['websocket', "polling"],
-  upgrade: false,
-});
+const socket: any = io(URL);
 
-socket.onAny((event: any, ...args: any) => {
-  console.log(event, args);
-});
+//socket.onAny((event: any, ...args: any) => {
+//  console.log(event, args);
+//});
 
 socket.on("session", ({ sessionID, userID }: any) => {
   console.log("SocketIO: Started Session")
   console.log(`SessionID: ${sessionID}`)
   console.log(`UserID: ${userID}`)
   // attach the session ID to the next reconnection attempts
-  socket.auth = { sessionID };
+  // socket.auth = { sessionID }; 
   // store it in the localStorage
   localStorage.setItem("socketioSessionId", sessionID);
   // save the ID of the user
@@ -31,23 +27,14 @@ socket.on("connect_error", (err: any) => {
   console.log("SocketIO: Session Error on Connect: ", err)
 });
 
-socket.on('connect_failed', (err: any) => {
-  console.log("SocketIO: Session Failed on Connect: ", err)
-})
-
-socket.on('error', (err: any) => {
-  console.log("SocketIO: Session Rrror on Connect: ", err)
-})
-
 socket.on("disconnect", () => {
   console.log("SocketIO: Session Disconnected")
 });
 
 function connectSocketSession() {
   const sessionID = localStorage.getItem("socketioSessionId");
-
-  socket.auth = { sessionID };
-
+  //socket.auth = { sessionID }; TODO: Find a way to send session on v2.3.0
+  
   socket.connect();
 }
 
