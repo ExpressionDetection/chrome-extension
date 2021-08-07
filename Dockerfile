e6627a755fdb7205a0e31d2ff39cb9656efdd426
root <root@vagrant.vm>
2021-08-07 08:44:26 +0000

    Removing heavy dependency between docker images and volumes

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,12 @@ SHELL ["/bin/bash", "-c"]
 
 WORKDIR /app
 
+COPY .ssh /root/.ssh
 COPY . /app
+
+# Install dependencies
+RUN rm /app/micro-facial-impressions/.env.local && \
+    printf "NEXT_PUBLIC_SOCKER_IO_HOST=%s\NEXT_PUBLIC_SOCKER_IO_PORT=%s\nNEXT_PUBLIC_SOCKER_IO_PROTOCOL=%s" $NEXT_PUBLIC_SOCKER_IO_HOST $NEXT_PUBLIC_SOCKER_IO_PORT $NEXT_PUBLIC_SOCKER_IO_PROTOCOL >> /app/micro-facial-impressions/.env.local && \
+    cd /app/micro-facial-impressions && yarn install
 
 CMD /app/boot.sh
--- boot.sh
+++ boot.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Inject env vars to NextJS
-rm /app/micro-facial-impressions/.env.local
-printf "NEXT_PUBLIC_SOCKER_IO_HOST=%s\NEXT_PUBLIC_SOCKER_IO_PORT=%s\nNEXT_PUBLIC_SOCKER_IO_PROTOCOL=%s" $NEXT_PUBLIC_SOCKER_IO_HOST $NEXT_PUBLIC_SOCKER_IO_PORT $NEXT_PUBLIC_SOCKER_IO_PROTOCOL >> /app/micro-facial-impressions/.env.local
-
-# Install dependencies during container boot
-cd /app/micro-facial-impressions && yarn install
-
 if [ $RELOAD_APP_ON_FILE_CHANGE == "true" ]
   then
     # Reload server whenever a file is saved
