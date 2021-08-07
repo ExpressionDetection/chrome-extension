8daf6a6d1f88163ec2abb30672b565a1af7ac421
root <root@vagrant.vm>
2021-08-07 09:58:35 +0000

    fix boot

--- boot.sh
+++ boot.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Install dependencies
-cd /app/micro-facial-impressions && yarn install 
-
 if [ $RELOAD_APP_ON_FILE_CHANGE == "true" ]
   then
+    # Install dependencies
+    cd /app/micro-facial-impressions && yarn install 
     # Reload server whenever a file is saved
     cd /app/micro-facial-impressions && yarn run dev
   else
